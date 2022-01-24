196f5ab81ef0a9e4e42d39398ed52e5691f40d62
Peter Dippold <peter.dippold@gmail.com>
2022-01-24 16:15:34 +0100

    Uncomment functions

--- utils.py
+++ utils.py
@@ -1,29 +1,28 @@
 import logging
 
-#import tensorflow.compat.v1 as tf
-import tensorflow as tf
+import tensorflow.compat.v1 as tf
 from object_detection.inputs import train_input
 from object_detection.protos import input_reader_pb2
 from object_detection.builders.dataset_builder import build as build_dataset
 from object_detection.utils.config_util import get_configs_from_pipeline_file
-#from waymo_open_dataset import dataset_pb2 as open_dataset
+from waymo_open_dataset import dataset_pb2 as open_dataset
 
 
-# def get_dataset(tfrecord_path, label_map='label_map.pbtxt'):
-#     """
-#     Opens a tf record file and create tf dataset
-#     args:
-#       - tfrecord_path [str]: path to a tf record file
-#       - label_map [str]: path the label_map file
-#     returns:
-#       - dataset [tf.Dataset]: tensorflow dataset
-#     """
-#     input_config = input_reader_pb2.InputReader()
-#     input_config.label_map_path = label_map
-#     input_config.tf_record_input_reader.input_path[:] = [tfrecord_path]
+def get_dataset(tfrecord_path, label_map='label_map.pbtxt'):
+    """
+    Opens a tf record file and create tf dataset
+    args:
+      - tfrecord_path [str]: path to a tf record file
+      - label_map [str]: path the label_map file
+    returns:
+      - dataset [tf.Dataset]: tensorflow dataset
+    """
+    input_config = input_reader_pb2.InputReader()
+    input_config.label_map_path = label_map
+    input_config.tf_record_input_reader.input_path[:] = [tfrecord_path]
     
-#     dataset = build_dataset(input_config)
-#     return dataset
+    dataset = build_dataset(input_config)
+    return dataset
 
 
 def get_module_logger(mod_name):
@@ -54,37 +53,37 @@ def get_train_input(config_path):
     dataset = train_input(train_config, train_input_config, configs['model'])
     return dataset
 
-# def parse_frame(frame, camera_name='FRONT'):
-#     """ 
-#     take a frame, output the bboxes and the image
+def parse_frame(frame, camera_name='FRONT'):
+    """ 
+    take a frame, output the bboxes and the image
 
-#     dataset = tf.data.TFRecordDataset(FILENAME, compression_type='')
-#       for data in dataset:
-#       frame = open_dataset.Frame()
-#       frame.ParseFromString(bytearray(data.numpy()))
+    dataset = tf.data.TFRecordDataset(FILENAME, compression_type='')
+      for data in dataset:
+      frame = open_dataset.Frame()
+      frame.ParseFromString(bytearray(data.numpy()))
     
-#     args:
-#       - frame [waymo_open_dataset.dataset_pb2.Frame]: a waymo frame, contains images and annotations
-#       - camera_name [str]: one frame contains images and annotations for multiple cameras
+    args:
+      - frame [waymo_open_dataset.dataset_pb2.Frame]: a waymo frame, contains images and annotations
+      - camera_name [str]: one frame contains images and annotations for multiple cameras
     
-#     returns:
-#       - encoded_jpeg [bytes]: jpeg encoded image
-#       - annotations [protobuf object]: bboxes and classes
-#     """
-#     # get image
-#     images = frame.images
-#     for im in images:
-#         if open_dataset.CameraName.Name.Name(im.name) != camera_name:
-#             continue
-#         encoded_jpeg = im.image
+    returns:
+      - encoded_jpeg [bytes]: jpeg encoded image
+      - annotations [protobuf object]: bboxes and classes
+    """
+    # get image
+    images = frame.images
+    for im in images:
+        if open_dataset.CameraName.Name.Name(im.name) != camera_name:
+            continue
+        encoded_jpeg = im.image
     
-#     # get bboxes
-#     labels = frame.camera_labels
-#     for lab in labels:
-#         if open_dataset.CameraName.Name.Name(lab.name) != camera_name:
-#             continue
-#         annotations = lab.labels
-#     return encoded_jpeg, annotations
+    # get bboxes
+    labels = frame.camera_labels
+    for lab in labels:
+        if open_dataset.CameraName.Name.Name(lab.name) != camera_name:
+            continue
+        annotations = lab.labels
+    return encoded_jpeg, annotations
 
 
 def int64_feature(value):
